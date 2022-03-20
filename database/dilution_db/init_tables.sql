bf45b4a9d838a7a440796b5e295b4debbc7ad24e
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-20 09:29:14 +0100

    work on db schema
    incomplete tables and references

--- database/dilution_db/init_tables.sql
+++ database/dilution_db/init_tables.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS outstanding_shares(
     CONSTRAINT fk_company_id
         FOREIGN KEY (company_id)
             REFERENCES companies(id)
-) 
+); 
 
 CREATE TABLE IF NOT EXISTS net_cash_and_equivalents(
     company_id SERIAL,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS net_cash_and_equivalents(
     CONSTRAINT fk_company_id
         FOREIGN KEY (company_id)
             REFERENCES companies(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS net_cash_and_equivalents_excluding_financing(
     company_id SERIAL,
@@ -51,35 +51,84 @@ CREATE TABLE IF NOT EXISTS net_cash_and_equivalents_excluding_financing(
     CONSTRAINT fk_company_id
         FOREIGN KEY (company_id)
             REFERENCES companies(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS capital_raise(
     company_id SERIAL,
+    shelf_id SERIAL,
     kind VARCHAR(255),
-    amount BIGINT,
+    amount_usd BIGINT,
+    form_type VARCHAR(255),
+    form_acn VARCHAR(255),
+    
 
     CONSTRAINT fk_company_id
         FOREIGN KEY (company_id)
             REFERENCES companies(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS underwriters(
     underwriter_id SERIAL PRIMARY KEY,
     underwriter_name VARCHAR(255)
-)
+);
 
-CREATE TABLE IF NOT EXISTS shelf_filing(
+CREATE TABLE IF NOT EXISTS shelf(
+    shelf_id SERIAL PRIMARY KEY,
     company_id SERIAL,
-    acn_number VARCHAR(255) NOT NULL PRIMARY KEY,
+    acn_number VARCHAR(255) NOT NULL,
     form_type VARCHAR(255) NOT NULL,
-    is_effective BOOLEAN,
-    deal_size BIGINT,
-    underwritter SERIAL,
-      
+    shelf_capacity BIGINT,
+    underwriter_id SERIAL,
+    total_amount_raised BIGINT,
+    total_amount_raised_unit VARCHAR(255) DEFAULT USD,
+    effect_date DATE,
+    last_update DATE,
+    expiry DATE,
+    filing_date DATE,
+
     CONSTRAINT fk_company_id
         FOREIGN KEY (company_id)
             REFERENCES companies(id),
     CONSTRAINT fk_underwriter_id
         FOREIGN KEY (underwriter_id)
             REFERENCES underwriters(underwriter_id)
+);
+
+CREATE TABLE IF NOT EXISTS securit
+
+
+
+CREATE TABLE IF NOT EXISTS offerings(
+    offering_id SERIAL PRIMARY KEY,
+    company_id SERIAL,
+    acn_number VARCHAR(255) NOT NULL,
+    inital_form_type VARCHAR(255) NOT NULL,
+    underwriter_id SERIAL,
+    final_offering_amount_usd BIGINT,
+    anticipated_offering_amount_usd BIGINT,
+    expiry DATE,
+    filing_date DATE,
+      
+    CONSTRAINT fk_company_id
+        FOREIGN KEY (company_id)
+            REFERENCES companies(id),
+    CONSTRAINT fk_underwriter_id
+        FOREIGN KEY (underwriter_id)
+            REFERENCES underwriters(underwriter_id),
+    CONSTRAINT fk_filing_status_id
+        FOREIGN KEY (filing_status_id)
+            REFERENCES filing_status(filing_status_id)
+);
+
+CREATE TABLE IF NOT EXISTS filing_status(
+    id SERIAL PRIMARY KEY,
+    status_name VARCHAR(255)
+)
+
+CREATE TABLE IF NOT EXISTS warrants(
+    company_id SERIAL,
+    filing_id SERIAL,
+    unit_per_warrant INT,
+    unit VARCHAR(255) DEFAULT shares,
+
 )
--- database/users_db/init_tables.sql
+++ database/users_db/init_tables.sql
@@ -6,7 +6,6 @@ BEGIN
     END LOOP;
 END $$;
 
--- user_part
 CREATE TABLE IF NOT EXISTS users (
 	id SERIAL PRIMARY KEY,
 	username VARCHAR(255) UNIQUE,

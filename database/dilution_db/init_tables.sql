9680c92193925fb484108d1f8c672609f3865861
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-20 19:30:09 +0100

    updated db schema to fix polymorphism

--- database/dilution_db/init_tables.sql
+++ database/dilution_db/init_tables.sql
@@ -5,6 +5,7 @@ BEGIN
         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
     END LOOP;
 END $$;
+
 --  TODO: add primary keys to most tables 
 CREATE TABLE IF NOT EXISTS companies (
     id SERIAL PRIMARY KEY,
@@ -53,26 +54,27 @@ CREATE TABLE IF NOT EXISTS net_cash_and_equivalents_excluding_financing(
             REFERENCES companies(id)
 );
 
-CREATE TABLE IF NOT EXISTS capital_raise(
-    company_id SERIAL,
-    shelf_id SERIAL,
-    kind VARCHAR(255),
-    amount_usd BIGINT,
-    form_type VARCHAR(255),
-    form_acn VARCHAR(255),
+-- CREATE TABLE IF NOT EXISTS capital_raise(
+--     company_id SERIAL,
+--     shelf_id SERIAL,
+--     kind VARCHAR(255),
+--     amount_usd BIGINT,
+--     form_type VARCHAR(255),
+--     form_acn VARCHAR(255),
     
 
-    CONSTRAINT fk_company_id
-        FOREIGN KEY (company_id)
-            REFERENCES companies(id)
-);
+--     CONSTRAINT fk_company_id
+--         FOREIGN KEY (company_id)
+--             REFERENCES companies(id)
+-- );
 
 CREATE TABLE IF NOT EXISTS underwriters(
     underwriter_id SERIAL PRIMARY KEY,
     underwriter_name VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS shelf(
+-- s-3, f-3 or f-6
+CREATE TABLE IF NOT EXISTS shelfs(
     shelf_id SERIAL PRIMARY KEY,
     company_id SERIAL,
     acn_number VARCHAR(255) NOT NULL,
@@ -94,10 +96,13 @@ CREATE TABLE IF NOT EXISTS shelf(
             REFERENCES underwriters(underwriter_id)
 );
 
-CREATE TABLE IF NOT EXISTS securit
 
 
-
+-- general category: offering is part of a registration filings like a shelsn, s-1, f-1.
+-- Details are either specified in the registration statement or in an ammendment pursuant to 424,
+-- A "POS AM", or ?
+-- describes a single security type being offered and its details and references the registration_filing
+-- it is specified or incorporated by reference into
 CREATE TABLE IF NOT EXISTS offerings(
     offering_id SERIAL PRIMARY KEY,
     company_id SERIAL,
@@ -106,7 +111,7 @@ CREATE TABLE IF NOT EXISTS offerings(
     underwriter_id SERIAL,
     final_offering_amount_usd BIGINT,
     anticipated_offering_amount_usd BIGINT,
-    expiry DATE,
+    filing_status SERIAL,
     filing_date DATE,
       
     CONSTRAINT fk_company_id
@@ -123,12 +128,94 @@ CREATE TABLE IF NOT EXISTS offerings(
 CREATE TABLE IF NOT EXISTS filing_status(
     id SERIAL PRIMARY KEY,
     status_name VARCHAR(255)
-)
+);
 
 CREATE TABLE IF NOT EXISTS warrants(
-    company_id SERIAL,
-    filing_id SERIAL,
-    unit_per_warrant INT,
-    unit VARCHAR(255) DEFAULT shares,
+    id SERIAL PRIMARY KEY,
+    warrant_name VARCHAR(255),
+    offering_id SERIAL,
+    shelf_id SERIAL,
+    acn_number VARCHAR(255),
+    amount_usd_equivalent BIGINT,
+    amount_issued BIGINT,
+    shares_per_unit FLOAT,
+    exercise_price FLOAT,
+    expiration_date DATE,
+    
+    CHECK (shelf_id or offering_id),
 
-)
+    CONSTRAINT fk_offering_id
+        FOREIGN KEY (offering_id)
+            REFERENCES offerings(offering_id),
+    CONSTRAINT fk_shelf_id
+        FOREIGN KEY (shelf_id)
+            REFERENCES shelfs(shelf_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS convertible__note_warrants(
+    id SERIAL PRIMARY KEY,
+    note_warrant_name VARCHAR(255),
+    offering_id VARCHAR(255),
+    shelf_id VARCHAR(255),
+    exercise_price FLOAT,
+    amount_issued BIGINT,
+    price_protection INT,
+    issue_date DATE,
+    exercisable_date DATE,
+    last_update DATE,
+    additional_notes VARCHAR(1000),
+
+    CHECK (shelf_id or offering_id),
+
+    CONSTRAINT fk_offering_id
+        FOREIGN KEY (offering_id)
+            REFERENCES offerings(offering_id),
+    CONSTRAINT fk_shelf_id
+        FOREIGN KEY (shelf_id)
+            REFERENCES shelfs(shelf_id)
+);
+
+CREATE TABLE IF NOT EXISTS convertible_notes(
+    id SERIAL PRIMARY KEY,
+    offering_id VARCHAR(255),
+    shelf_id VARCHAR(255),
+
+    CHECK (shelf_id or offering_id),
+
+    CONSTRAINT fk_offering_id
+    FOREIGN KEY (offering_id)
+        REFERENCES offerings(offering_id),
+    CONSTRAINT fk_shelf_id
+        FOREIGN KEY (shelf_id)
+            REFERENCES shelfs(shelf_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS atm(
+    id SERIAL PRIMARY KEY,
+    offering_id VARCHAR(255),
+    shelf_id VARCHAR(255),
+    total_capacity BIGINT,
+    underwriter_id SERIAL,
+    agreement_start_date DATE,
+
+    CHECK (shelf_id or offering_id),
+
+    CONSTRAINT fk_offering_id
+    FOREIGN KEY (offering_id)
+        REFERENCES offerings(offering_id),
+    CONSTRAINT fk_shelf_id
+        FOREIGN KEY (shelf_id)
+            REFERENCES shelfs(shelf_id),
+    CONSTRAINT fk_underwriter_id
+      FOREIGN KEY (underwriter_id)
+        REFERENCES underwriters(underwriter_id)
+
+);
+
+-- to get all offerings from a certain company, I..
+-- select all shelfs/ offerings that have wanted company_id (SELECT WHERE)
+-- then SELECT * JOIN on each securities table with shelf/offerings id
+-- convertible_notes, atm
+

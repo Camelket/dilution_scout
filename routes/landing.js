e9b2b539fa353249a69821da0c4157db4eaf55d7
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-21 09:32:07 +0100

    added to landing page

--- routes/landing.js
+++ routes/landing.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const landingPanel = {"src": "C:\Users\Olivi\OneDrive\Github_Projects\dilution_scout\resources\backgrounds\cardboard-tetris.jpg",
+                      "title_text": "Welcome to DilutionScout!",
+                      "description": "Free and transparant dilution tracking."}
+const contentPanel = [{"id_text": "cp1", "title": "TITLE", "text": "TEXT", "id_image": "cp1img", "image_src": "C:\Users\Olivi\OneDrive\Github_Projects\dilution_scout\resources\backgrounds\abstract-light-circle.jpg"}]
+
 router.use("/landing", function(req, res, next) {
     res.render("landing")
 })

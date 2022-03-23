9ffe745044b7c4901c6bed7c5b4bcbc0a656b341
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-24 00:11:45 +0100

    work on landing panels

--- routes/landing.js
+++ routes/landing.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const landingPanel = {"src": "\backgrounds\cardboard-tetris.jpg",
+const landingPanel = {"src": "/backgrounds/cardboard-tetris.jpg",
                       "title_text": "Welcome to DilutionScout!",
                       "description": "Free and transparant dilution tracking."}
 const contentPanel = [{"id_text": "cp1",
                         "title": "TITLE",
                         "text": "TEXT",
                         "id_image": "cp1img",
-                        "image_src": "\backgrounds\abstract-light-circle.jpg"}]
+                        "image_src": "/public/backgrounds/abstract-light-circle.jpg"}]
 
 router.get("/landing", function(req, res, next) {
     res.render("landing", {"content_panels": contentPanel, "landing_panel": landingPanel})

cba601cd1c11a568709cd6bccdd136d3b89f59f0
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-24 15:43:10 +0100

    added tabs to /ticker/:id and some styling

--- routes/landing.js
+++ routes/landing.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const landingPanel = {"src": "/backgrounds/cardboard-tetris.jpg",
-                      "title_text": "Welcome to DilutionScout!",
-                      "description": "Free and transparant dilution tracking."}
-const contentPanel = [{"id_text": "cp1",
-                        "title": "TITLE",
-                        "text": "TEXT",
-                        // "id_image": "cp1img",
-                        "image_src": "/public/backgrounds/abstract-light-circle.jpg"}]
+const landingPanel = {
+  src: "/backgrounds/cardboard-tetris.jpg",
+  title_text: "Welcome to DilutionScout!",
+  description: "Free and transparant dilution tracking.",
+};
+const contentPanel = [
+  {
+    id_text: "cp1",
+    title: "TITLE",
+    text: "TEXT",
+    // "id_image": "cp1img",
+    image_src: "/public/backgrounds/abstract-light-circle.jpg",
+  },
+];
 
-router.get("/landing", function(req, res, next) {
-    res.render("landing", {"content_panels": contentPanel, "landing_panel": landingPanel})
-})
+router.get("/landing", function (req, res, next) {
+  res.render("landing", {
+    content_panels: contentPanel,
+    landing_panel: landingPanel,
+  });
+});
 
-
-module.exports = router
+module.exports = router;

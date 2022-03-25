5b782075021113766391b454060e9cc7f2172c48
Camelket <34602676+Camelket@users.noreply.github.com>
2022-03-25 23:50:17 +0100

    corrected wrong code for dilution_db CRUD functions
    - got all functions needed to start building chart1 from db
    - now: write and test chart1,.then make tables and CRUD for chart2.then do aggregation and population.then look into how to setup the website with a host and how i would do automatic  updates on the db.

--- database/dilution_db/populate_dilution_db.js
+++ database/dilution_db/populate_dilution_db.js
@@ -13,7 +13,8 @@ const data = {
         "cik": "000032019",
         "sic": "8040",
         "symbol": "AXNS",
-        "company_name": "Random ltd"
+        "name": "Random ltd",
+        "description": "lorem ipsum company does lorem ipsum in lorem with ipsum"
     },
     "sics":{
         "sic": "8040",
@@ -37,14 +38,46 @@ const data = {
 }
 
 const populate = async function() {
-    await createSIC(db, data["sics"]["sic"],data["sics"]["sector"], data["sics"]["industry"])
-}
+    try {
+        await createSIC(db, data["sics"]["sic"],data["sics"]["sector"], data["sics"]["industry"])
+    }catch(e) {}
+    try{
+        await createCompany(db, data["company"]["cik"], data["company"]["sic"], data["company"]["symbol"], data["company"]["name"], data["company"]["description"]);
+    }catch(e) {}
+    let id;
+    try{id = await db.one("SELECT id FROM companies WHERE symbol = $1", [data["company"]["symbol"]])}
+    catch(e){console.log(`logging ${e}`)}
+    // just use a regular for loop ...
+    for (let i=0; i < data["outstanding_shares"].length; i++){
+        try{
+            console.log(data["outstanding_shares"][i])
+            let p = data["outstanding_shares"][i]
+            await createOutstandingShares(db, id["id"], p["instant"], p["amount"])}
+        catch(e) {console.log(`${e} Couldnt create outstanding shares`)}
+    }
+    for (let i=0; i < data["net_cash_and_equivalents"].length; i++){
+        try{
+            console.log(data["net_cash_and_equivalents"][i])
+            let p = data["net_cash_and_equivalents"][i]
+            await createNetCashAndEquivalents(db, id["id"], p["instant"], p["amount"])}
+        catch(e) {console.log(`${e} Couldnt create outstanding shares`)}
+}}
 
 const testContent = async function() {
     let sics = await db.any("SELECT * FROM sics")
     console.log(sics)
+    let companies = await db.any("SELECT * FROM companies")
+    console.log(companies)
+    let outstanding_shares = await db.any("SELECT * FROM outstanding_shares")
+    console.log(outstanding_shares)
+    let net_cash_and_equivalents = await db.any("SELECT * FROM net_cash_and_equivalents")
+    console.log(net_cash_and_equivalents)
 }
-populate()
-testContent()
-
-
+// let tables;
+// setTimeout( async function (){
+    
+//     tables = await db.any("SELECT table_schema , table_name FROM information_schema.tables WHERE table_schema not in ('information_schema', 'pg_catalog') AND table_type = 'BASE TABLE' ORDER BY table_schema, table_name")
+//     setTimeout(() => console.log(tables), 2000)
+// }, 2000)
+setTimeout(() => {populate();}, 2000)
+setTimeout(() => {testContent()}, 3000)

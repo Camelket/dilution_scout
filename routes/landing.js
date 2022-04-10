7d8a6605628145903e00206d51ded119fee9c711
Camelket <34602676+Camelket@users.noreply.github.com>
2022-04-10 07:06:49 +0200

    wip hamburger menu

--- routes/landing.js
+++ routes/landing.js
@@ -6,19 +6,22 @@ const landingPanel = {
   title_text: "Welcome to DilutionScout!",
   description: "(WIP) Attempting Free and transparant dilution tracking.",
 };
-const contentPanel = [
-  {
-    id_text: "cp1",
-    title: "TITLE",
-    text: "TEXT",
-    // "id_image": "cp1img",
-    image_src: "/public/backgrounds/abstract-light-circle.jpg",
-  },
-];
+const contentPanel = {
+  
+  "features": {
+    "Outstanding Shares": `Tracks outstanding common shares based on
+                           10-K and 10-Q filings.`,
+    "Cash Position": `net cash and equivalents based on 10-K and 10-Q filings,
+                      excluding noncurrent equivalents.`,
+    "Filing Links": `Chronological and categorised Filings with direct links to the main document`,
+    "Cash Burn": `Cash burn and days to no cash base on 10-K and 10-Q filings and the Cash Position`,
+    "User Accounts": `Working password and email or Google login. No user features yet`
+  }}
+
 
 router.get("/landing", function (req, res, next) {
   res.render("landing", {
-    content_panels: contentPanel,
+    content_panel: contentPanel,
     landing_panel: landingPanel,
   });
 });

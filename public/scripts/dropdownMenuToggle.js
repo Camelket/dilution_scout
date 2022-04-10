523bd00cfcabe2a47d54a7eeae9d67fa0d60989c
Camelket <34602676+Camelket@users.noreply.github.com>
2022-04-10 09:29:34 +0200

    interim push

--- public/scripts/dropdownMenuToggle.js
+++ public/scripts/dropdownMenuToggle.js
@@ -3,14 +3,21 @@ function toggleMenu() {
     //- https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
     menu = document.getElementById("dropdownMedium").classList
     menu.toggle("hidden")
-    menu.toggle("show")
+
+    let burger = document.getElementById("hamburger_all").classList
+    if (!burger.contains("is_open")) {
+        burger.add("is_open") 
+    } else {
+        burger.toggle("is_open")
+    }
+    // menu.toggle("show")
 }
 
 window.onclick = function(event) {
 if (!event.target.matches('.dropdownNavToggle')) {
     let items = document.getElementsByClassName("dropdownItems");
     for (let i = 0; i < items.length; i++) {
-    let openDropdown = items[i];
+        let openDropdown = items[i];
         if (openDropdown.classList.contains("show")) {
         openDropdown.classList.toggle("hidden")
         openDropdown.classList.toggle("show")

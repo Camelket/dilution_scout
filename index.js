b6d94915d5258144ab174a306df9401696dcb65c
Camelket <34602676+Camelket@users.noreply.github.com>
2022-04-13 18:40:44 +0200

    removed unused dep. and changed index.js

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 require("dotenv").config()
-const express = require("express");
-const app = require("./app");
-const http = require("http");
 
-const port = process.env.PORT || '8000';
-app.set('port', port);
+if (process.env.NODE_ENV == "prod") {
+  const app = require("./app");
+  const http = require("http");
+  const https = require("https");
+  const fs = require("fs");
+  const port = process.env.PORT || '8000';
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/dilutionscout.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/dilutionscout.com/fullchain.pem")
+  };
+  console.log(port)
+  // app.set('port', port);
+  //app.listen(port)
+  // app.listen(port, () => {
+    //    console.log(`Listening to requests on http://localhost:${port}`);
+    //  });
+  https.createServer(options, app).listen(8080, "localhost");
+  http.createServer(app).listen(port, "localhost");
+}
+else {
+  const app = require("./app");
+  const http = require("http");
+  const { env } = require("process");
 
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
-// const server = http.createServer(app);
-// server.listen(port);
+  const port = process.env.PORT || '8000';
+  app.set('port', port);
 
+  app.listen(port, () => {
+      console.log(`Listening to requests on http://localhost:${port}`);
+    });
+  // const server = http.createServer(app);
+  // server.listen(port);
+  }
